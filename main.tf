e52c9f1c87340a77162732114b663389615d0ec3
Jorge Jimenez <jorgejimenez.globant@latam.com>
2020-09-22 00:24:19 -0500

    Terraform FMT

--- main.tf
+++ main.tf
@@ -124,8 +124,8 @@ resource "aws_lb_listener" "listener" {
 }
 
 resource "aws_ecs_service" "my_first_service" {
-  name            = "my-first-service"                             # Naming our first service
-  cluster         = "${aws_ecs_cluster.my_cluster.id}"             # Referencing our created Cluster
+  name            = "my-first-service"                       # Naming our first service
+  cluster         = "${aws_ecs_cluster.my_cluster.id}"       # Referencing our created Cluster
   task_definition = "${aws_ecs_task_definition.my_task.arn}" # Referencing the task our service will spin up
   launch_type     = "FARGATE"
   desired_count   = 3 # Setting the number of containers to 3
